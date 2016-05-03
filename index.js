dd941fcf1769a0149153f905ff160f2ae7afc351
3LOK <yoava@wix.com>
2016-05-03 18:39:15 +0300

    webpack -> babel for real library usage

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-import {DomainValidator} from "./src/DomainValidator.js"
-import {EmailValidator} from "./src/EmailValidator.js"
-
-export {DomainValidator as DomainValidator}
-export {EmailValidator as EmailValidator}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict"
+
+import {DomainValidator} from "./DomainValidator"
+import {EmailValidator} from "./EmailValidator"
+
+export {DomainValidator as DomainValidator}
+export {EmailValidator as EmailValidator}
